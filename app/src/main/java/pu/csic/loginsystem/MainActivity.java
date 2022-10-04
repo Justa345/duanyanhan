843507430ccad880e76773817908fccd7b11b5b2
student <s1092008@gm.pu.edu.tw>
2022-10-04 15:42:43 +0800

    1004

--- app/src/main/java/pu/csic/loginsystem/MainActivity.java
+++ app/src/main/java/pu/csic/loginsystem/MainActivity.java
@@ -1,16 +1,37 @@
 package pu.csic.loginsystem;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.id1:
+                Toast.makeText(getApplicationContext(),"物品1被選擇",Toast.LENGTH_LONG).show();
+                break;
+                case R.id.id2:
+                    Toast.makeText(getApplicationContext(),"物品2被選擇",Toast.LENGTH_LONG).show();
+                    break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
     }
 
 }
+
